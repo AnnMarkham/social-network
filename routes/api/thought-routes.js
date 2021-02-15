e08aad9a565a98c251038e7bd1ee27255d3d854f
AnnMarkham <ann.e.markham@outlook.com>
2021-02-14 21:15:13 -0700

    create reaction working

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { getAllThoughts, addThought } = require('../../controllers/thought-controller');
+const { getAllThoughts, addThought, addReaction } = require('../../controllers/thought-controller');
 
 // GET all thoughts: /api/thoughts
 router.route('/').get(getAllThoughts);
 
-// POST thought: /api/thoughts
+// POST thought: /api/thoughts/:userId
 router.route('/:userId').post(addThought);
+
+//Add Reaction  POST:  /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
 
 module.exports = router;
